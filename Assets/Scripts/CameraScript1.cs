e5681733bed0e280be9e5dbea53fc1d9e31bab9c
Johnny Pham <jpham009@gmail.com>
2019-02-07 03:48:21 -0600

    Initial commit

---
+++ Assets/Scripts/CameraScript1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript1 : MonoBehaviour
+{
+
+    public Transform player;
+    public float offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset, transform.position.y, transform.position.z);
+    }
+}
