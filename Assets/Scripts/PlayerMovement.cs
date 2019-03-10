1f351b32b6ff91e6c1f6e1709bff33056f622093
Johnny Pham <johnny.pham2@mavs.uta.edu>
2019-03-10 03:57:05 -0500

    Added double jump and falling drag
    - Press space again to jump higher. (double jump)
    - Hold the jump button and gravity will drag (slow fall)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,21 +19,17 @@ public class PlayerMovement : MonoBehaviour
     private float jumpTimeCounter;  //time to count how long player has pressed jump key
     private bool isJumping;         //bool to tell if player is jumping or not
     Animator anim;
+    private bool doubleJumped;      //tells if player has doublejumped or not 
+
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();   //get reference to 	Rigidbody2D component
         anim = GetComponent<Animator>();
+        doubleJumped = false;
     }
 
-    //private void FixedUpdate()
-    //{
-
-    //}
-
-
-    
-
+    // Updates once per frame
     private void Update()
     {
 
@@ -60,45 +56,65 @@ public class PlayerMovement : MonoBehaviour
         transform.position = Camera.main.ViewportToWorldPoint(pos);
 
 
-
-
         //here we set our player x velocity and y will not ne 
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         //here we set the isGrounded
         isGrounded = Physics2D.OverlapCircle(feetPos.position, circleRadius, whatIsGround);
-        //we check if isGrounded is true and we pressed Space button
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            isJumping = true;                       //we set isJumping to true
-            jumpTimeCounter = jumpTime;                 //set jumpTimeCounter
-            rb.velocity = Vector2.up * jumpForce;       //add velocity to player
-        }
 
         if (isGrounded == false)
         {
             anim.SetInteger("condition", 2);
         }
-       
- 
+        if (isGrounded == true)
+        {
+            doubleJumped = false;
+        }
+        
+
+        // User input for Jump and Double Jump 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded == true)
+            {
+                isJumping = true;                       //we set isJumping to true
+                jumpTimeCounter = jumpTime;                 //set jumpTimeCounter
+                rb.velocity = Vector2.up * jumpForce;       //add velocity to player
+            }
+            else if (isGrounded == false && doubleJumped == false)
+            {
+                isJumping = true;
+                jumpTimeCounter = jumpTime;                 
+                rb.velocity = Vector2.up * jumpForce;       
+                doubleJumped = true;                   // double jump 
+            }
+        }
 
         //if Space key is pressed and isJumping is true
-        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        if (Input.GetKey(KeyCode.Space))
         {
-            
-            if (jumpTimeCounter > 0)                    //we check if jumpTimeCounter is more than 0
+            if (isJumping == false)
             {
-                rb.velocity = Vector2.up * jumpForce;   //add velocity
-                jumpTimeCounter -= Time.deltaTime;      //reduce jumpTimeCounter by Time.deltaTime
+                rb.drag = 8;  // Gravity jump (drag when falling) 
             }
-            else                                        //if jumpTimeCounter is less than 0
+            
+            if (isJumping == true)
             {
-                isJumping = false;                      //set isJumping to false
+                if (jumpTimeCounter > 0)                    //we check if jumpTimeCounter is more than 0
+                {
+                    rb.velocity = Vector2.up * jumpForce;   //add velocity
+                    jumpTimeCounter -= Time.deltaTime;      //reduce jumpTimeCounter by Time.deltaTime
+                }
+                else                                        //if jumpTimeCounter is less than 0
+                {
+                    isJumping = false;                      //set isJumping to false
+                }
             }
         }
 
         if (Input.GetKeyUp(KeyCode.Space))              //if we unpress the Space key
         {
+            rb.drag = 0;                                // back to normal gravity
             isJumping = false;                          //set isJumping to false
         }
 
