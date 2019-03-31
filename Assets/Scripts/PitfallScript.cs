4f2453955b21af962fca73324b4ad70a4b5e0ce7
Johnny Pham <jpham009@gmail.com>
2019-03-30 23:55:33 -0500

    Player Object Destroyed on Pitfall

---
+++ Assets/Scripts/PitfallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallScript : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("entered " + col.name);
+        Destroy(player);
+           
+    }
+}
