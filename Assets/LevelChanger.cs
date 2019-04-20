37cd2b9234cfcf510ef5cb0015d7a7c31f93086c
LinglingJ <Jessica.Bao@mavs.uta.edu>
2019-04-20 01:01:16 -0500

    Stage and Trans
    Press "k" to switch to level 2 from level 1

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex; 
+        animator.SetTrigger("fadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
