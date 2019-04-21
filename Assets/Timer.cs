20cab7f9faa5fd8d7f7dda2ba19531fe7ab545ac
LinglingJ <Jessica.Bao@mavs.uta.edu>
2019-04-21 10:57:32 -0500

    score

---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float t;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        t = Time.time - startTime;
+
+    }
+}
