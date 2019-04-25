3f86c62ae1645a79011d7a9e1eddc4577e876b7a
lphanvilay <linda.phanvilay@mavs.uta.edu>
2019-04-24 21:26:23 -0500

    updated enemy movements
    flips directions

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -21,7 +21,11 @@ public class Enemy_Movement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {     if(player.position.x < transform.position.x){
+            transform.localScale = new Vector3(1,1,1);
+        }else if(player.position.x > transform.position.x){
+            transform.localScale = new Vector3(-1,1,1);
+        }
         if(Vector2.Distance(transform.position, player.position) > stoppingDistance) {
         	transform.position = Vector2.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
         } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance){
@@ -36,7 +40,6 @@ public class Enemy_Movement : MonoBehaviour
         } else {
         	timeBtwShots = Time.deltaTime;
         }
-            if(Mathf.Abs (player.position.x - transform.position.x) < float.Epsilon)
-                            transform.eulerAngles = new Vector3(0, 180, 0);
+
     }
 }
