e1ced265deb617731a5749bd1906b6391edca5d8
Johnny Pham <jpham009@gmail.com>
2019-05-15 01:32:46 -0500

    update

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class LoadLevel : MonoBehaviour
-{
+//public class LoadLevel : MonoBehaviour
+//{
 
-    public GameObject TransitionExit;
-    public string levelToLoad;
+//    public GameObject TransitionExit;
+//    public string levelToLoad;
 
-    void Start()
-    {
-        TransitionExit.SetActive(false);
-    }
+//    void Start()
+//    {
+//        TransitionExit.SetActive(false);
+//    }
 
-    // Update is called once per frame
-    void OnTriggerStay(Collider plyr)
-    {
-        if (plyr.gameObject.tag == "Player")
-        {
-            TransitionExit.SetActive(true);
-            if (TransitionExit.activeInHierarchy==true)
-            {
-                SceneManager.LoadScene(levelToLoad);
-            }
-        }
-    }
-    void OnTriggerExit(Collider plyr)
-    {
-        if (plyr.gameObject.tag == "Player")
-        {
-            TransitionExit.SetActive(false);
-        }
-    }
-}
+//    // Update is called once per frame
+//    void OnTriggerStay(Collider plyr)
+//    {
+//        if (plyr.gameObject.tag == "Player")
+//        {
+//            TransitionExit.SetActive(true);
+//            if (TransitionExit.activeInHierarchy==true)
+//            {
+//                SceneManager.LoadScene(levelToLoad);
+//            }
+//        }
+//    }
+//    void OnTriggerExit(Collider plyr)
+//    {
+//        if (plyr.gameObject.tag == "Player")
+//        {
+//            TransitionExit.SetActive(false);
+//        }
+//    }
+//}
